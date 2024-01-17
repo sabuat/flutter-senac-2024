3b21a4062bb7f66ef3900722101b7ee0d6cae23d
Sabuat Urbina Ribeiro <90584688+sabuat@users.noreply.github.com>
2024-01-16 21:31:45 -0300

    Update First.dart
    Second Day Lecture

--- First.dart
+++ First.dart
@@ -48,3 +48,60 @@ void main() {
   number.removeAt(2);
   print (number);
 } 
+
+###
+
+void main() { 
+  var num1 = 101;
+  var num2 = 2;
+  var res = 0;
+  double res2 = 0.0;
+  res = num1 + num2;
+  print("Suma: ${res}");
+  res = num1 - num2;
+  print("Resta: ${res}");
+  res = num1 * num2;
+  print("Multiplicação: ${res}");
+  res2 = num1 / num2;
+  print("Divisão: ${res2}");
+  res = num1 ~/ num2;
+  print("Divisão Enteiro: ${res}");
+  res = num1 % num2;
+  print("Resto: ${res}");
+  res = num1++;
+  print("Incremento: ${res}");
+  res = num2--;
+  print("Decremento: ${res}");
+}
+
+### 
+
+void main() { 
+ var a = 10;
+ var b = 12;
+ var res = (a<b)&&(b!=0);
+ print(res);
+}
+
+### 
+
+void main() { 
+ var a = 10.365;
+ var c = 8;
+ var b = 0;
+ b = a.ceil();
+ print(b);
+ b = a.floor();
+ print(b);
+ b = c.abs();
+ print(b);
+}
+
+### 
+
+void main() { 
+ var a = "Araguaney Escola digital";
+ print(a.toLowerCase());
+ print(a.toUpperCase());
+ print(a.trim());
+}
